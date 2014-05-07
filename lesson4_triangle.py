cdd2ee56925b70786ce3012f578987bff2dbd7e1
jhgrng <jhgrng@gmail.com>
2014-05-07 15:12:51 +0200

    Completed L4 Triangle problem.

--- lesson4_triangle.py
+++ lesson4_triangle.py
@@ -8,8 +8,29 @@
 
 def solution(A):
     '''
+    Find if there's a triplet(combination of 3 values) that's triangular.
+
+    >>> solution([10, 2, 5, 1, 8, 2])
+    1
+
+    >>> [10, 50, 5, 1]
+    0
 
     '''
     
 
     # declare variables
+    A = sorted(A)
+    n = len(A)
+
+
+    # search for triplets
+    for i in xrange(1, n - 1):
+        if A[-i] + A[-i - 1] > A[-i - 2] and \
+        A[-i] + A[-i - 2] > A[-i - 1] and \
+        A[-i - 2] + A[-i - 1] > A[-i]:
+            return 1
+
+
+    # return the result
+    return 0
