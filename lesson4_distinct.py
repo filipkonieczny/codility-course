b07d3b1b45b2abcb6c4122faa2fe7d882638ae7e
jhgrng <jhgrng@gmail.com>
2014-05-07 14:03:28 +0200

    L4 Distinct sorting problem.

---
+++ lesson4_distinct.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# Lesson 3 - Prefix Sums - Distinct
+# https://codility.com/demo/take-sample-test/distinct
+
+
+def solution(A, B, K):
+    '''
+    
+    '''
+    
+
+    #
