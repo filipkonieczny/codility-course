d16c4911aa22cb6500218a7c3d03887e1930bf0a
jhgrng <jhgrng@gmail.com>
2015-02-04 15:41:47 +0100

    Reviewed Lesson 1: 'Tape Equilibrium'

--- lesson01/lesson1_tape_equilibrium.py
+++ lesson01/lesson1_tape_equilibrium.py
@@ -2,54 +2,38 @@
 # encoding: utf-8
 
 
-# Lesson 1 - Time Complexity - TapeEquilibrium
+# Lesson 1 - Time Complexity - Tape Equilibrium
 # https://codility.com/demo/take-sample-test/tape_equilibrium
 
 
-def solution(A):
+def solution(array):
     '''
     This function splits an array of integers into 2 tapes.
     Calculates the absolute difference between the sum of those 2 tapes.
     Returns the smallest difference.
-    
-    
+
+
     (list of integers) -> int
-    
-    
+
+
     >>> solution([3, 1, 2, 4, 3])
     1
-    
+
     Because abs((3 + 1 + 2) - (4 + 3)) = 1
-    
+
     '''
-    
-    
-    # declare variables
-    difference = 0
-    result = 0
-    sum_after_p = 0
-    sum_before_p = A[0]
-    
-    
-    # sum all elements
-    for i in range(1, len(A)):
-        sum_after_p += A[i]
 
+    prev_sum = array[0]
+    next_sum = sum(array) - array[0]
+    result = abs(prev_sum - next_sum)
 
-    # calculate the value of result for the 1st element
-    result = abs(sum_before_p - sum_after_p)
-    
-    
-    # calculate the difference
-    for i in range(1, len(A)):
-        difference = abs(sum_before_p - sum_after_p)
-        
+    for i in range(1, len(array)):
+        difference = abs(prev_sum - next_sum)
+
         if difference < result:
             result = difference
-        
-        sum_before_p += A[i]
-        sum_after_p -= A[i]
-    
-    
-    # return the result
+
+        prev_sum += array[i]
+        next_sum -= array[i]
+
     return result
