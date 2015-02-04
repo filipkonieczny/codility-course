2cea356e004e309c8040badf590aa4e17f36df57
jhgrng <jhgrng@gmail.com>
2015-02-04 13:56:10 +0100

    Reviewed Lesson 1: 'Perm Missing Element'

--- lesson01/lesson1_perm_missing_elem.py
+++ lesson01/lesson1_perm_missing_elem.py
@@ -6,35 +6,19 @@
 # https://codility.com/demo/take-sample-test/perm_missing_elem
 
 
-def solution(A):
+def solution(array):
     '''
     Find the missing number in an array of N different integers.
     The range of integers is [1, (N + 1)].
     Find the missing element.
-    
-    
+
     (list of integers) -> int
-    
-    
+
     >>> solution([2, 3, 1, 5])
     4
-    
+
     '''
-    
-    
-    # declare variables
-    sum_elements = 0
-    sum_series = 0
-    n = len(A) + 1
-    
-    
-    # sum all of the elements of the list
-    for i in A:
-        sum_elements += i
-    
-    # sum the series
-    sum_series = n * (n + 1) / 2
-    
-    
-    # return the missing number
-    return sum_series - sum_elements
+
+    array_sum = sum(array)
+    expected_sum = sum(xrange(1, len(array) + 2))
+    return expected_sum - array_sum
